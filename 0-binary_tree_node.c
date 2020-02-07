5356375bd45b049e07c33c8523a9e5ea1173cc35
KFredlund <katihomes@gmail.com>
2020-02-07 18:14:57 +0000

    Create new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,51 +9,12 @@ binary_tree_t *create_node(int value);
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = create_node(value);
-	binary_tree_t *current;
-
-	if (new_node == NULL)
-		return (NULL);
-	if (parent == NULL)
-		parent = new_node;
-	else
-	{
-		current = parent;
-		while (1)
-		{
-			if (parent->n > value)
-			{
-				current = current->left;
-				if (current == NULL)
-				{
-					parent->left = new_node;
-					return (new_node);
-				}
-			}
-			else
-			{
-				current = current->right;
-				if (current == NULL)
-				{
-					parent->right = new_node;
-					return (new_node);
-				}
-			}
-		}
-	}	
-	return (new_node);
-}
-/**
-* create_node - A helper function that creates a node
-* @value: value of new node
-*
-* Return: New node
-*/
-binary_tree_t *create_node(int value)
-{
 	binary_tree_t *new_node;
 
 	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
